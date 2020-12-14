de7f5f4f72b34e8bb4154e0af4e6eb2733fa373e
niu-grandpa <2864103063@qq.com>
2020-12-14 20:33:23 +0800

    重写

--- src/components/spin/spin.js
+++ src/components/spin/spin.js
@@ -2,70 +2,54 @@
  * Spin 加载中
  * 用于页面和区块的加载中状态
  */
-let spinZIndex = 2010;
-
 Rabbit.prototype.Spin = {
     prefixCls: 'rbt-spin',
-    createInstance(_size, _text, _color) {
-        const enterAniCls = `${this.prefixCls}-fade-enter`;
-        const leaveAniCls = `${this.prefixCls}-fade-leave`;
-
-        const SpinFullscreen = document.createElement('div');
-        const SpinFixBox = document.createElement('div');
-        const SpinMainBox = document.createElement('div');
-
-        const SpinDot = document.createElement('span');
-
-        const SpinShowTextBox = document.createElement('div');
-        const SpinTextBox = document.createElement('div');
-        const SpinBorder = document.createElement('i');
-        const SpinText = document.createElement('span');
-
-        SpinFullscreen.className = `${this.prefixCls}-fullscreen ${this.prefixCls}-fullscreen-warpper`;
-        SpinFixBox.className = `${this.prefixCls}-fix ${this.prefixCls}-fullscreen`;
-        SpinMainBox.className = `${this.prefixCls}-main`;
-
-        SpinDot.className = `${this.prefixCls}-dot bg-${_color} ${_size}`;
-
-        SpinShowTextBox.className = `${this.prefixCls} ${this.prefixCls}-fix ${this.prefixCls}-show-text`;
-        SpinTextBox.className = `${this.prefixCls}-text`;
-        SpinBorder.className = `rbt-icon rbt-icon-loading-solid rbt-spin-loading ${_size}`;
-
-        SpinText.innerHTML = _text;
-
-        SpinFullscreen.style.zIndex = spinZIndex++;
-
-        SpinFullscreen.appendChild(SpinFixBox);
-        SpinFixBox.appendChild(SpinMainBox);
-        SpinShowTextBox.append(SpinTextBox);
-        SpinTextBox.append(SpinBorder, SpinText);
-
-        _text
-            ?
-            SpinMainBox.appendChild(SpinShowTextBox) :
-            SpinMainBox.appendChild(SpinDot);
-
-        document.body.appendChild(SpinFullscreen);
-
-        CSSTransition(SpinFixBox, 'in', enterAniCls, leaveAniCls, 250);
+    customContent() {
+        const spins = document.querySelectorAll('rb-spin');
+        spins.forEach(spin => {
+            if (spin.getAttribute('rb-type') !== 'dot') {
+                const container = document.createElement('span');
+                container.className = `${this.prefixCls}-main`;
+                container.innerHTML = spin.innerHTML;
+                spin.innerHTML = null;
+                spin.appendChild(container);
+            }
+        });
     },
+    show({ content } = {}) {
+        const Spin = document.createElement('rb-spin');
+        let SpinMain = document.createElement('span');
 
-    show({ size = 'large', text = '', color = 'primary' } = {}) {
-        this.createInstance(size, text, color);
-    },
+        Spin.setAttribute('rb-fix', 'true');
+        Spin.dataset.fullscreen = true;
+        Spin.style.position = 'fixed';
 
-    hide() {
-        const spin = document.querySelector(`.${this.prefixCls}-fullscreen`);
-        const spinFixBox = spin.querySelector(`.${this.prefixCls}-fix`);
+        SpinMain.className = `${this.prefixCls}-main`;
+        SpinMain.innerHTML = content ?
+            content :
+            `<rb-spin rb-type="dot" rb-size="large"></rb-spin>`;
+
+        Spin.appendChild(SpinMain);
+        document.body.appendChild(Spin);
 
         CSSTransition(
-            spinFixBox,
-            `out`,
-            `${this.prefixCls}-fade-enter`,
-            `${this.prefixCls}-fade-leave`,
+            Spin,
+            'in',
+            `${this.prefixCls}-fade-in`,
+            `${this.prefixCls}-fade-out`,
             250
         );
-
-        setTimeout(() => document.body.removeChild(spin), 250);
+    },
+    hide() {
+        const Spin = document.querySelector('rb-spin[data-fullscreen="true"]');
+        CSSTransition(
+            Spin,
+            'out',
+            `${this.prefixCls}-fade-in`,
+            `${this.prefixCls}-fade-out`,
+            250
+        );
+        setTimeout(() => document.body.removeChild(Spin), 250);
     },
 };
+Rabbit.prototype.Spin.customContent();
