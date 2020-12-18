8c7dcb9629ec69682ff07e48f73badb3a05b1fd1
niu-grandpa <2864103063@qq.com>
2020-12-18 12:10:29 +0800

    重构项目

--- src/mixins/scrollable.js
+++
@@ -1,9 +0,0 @@
-function bodyScrollable(flag) {
-    if (!flag) {
-        document.body.style.paddingRight = '17px';
-        document.body.style.overflow = 'hidden';
-    } else {
-        document.body.style.paddingRight = null;
-        document.body.style.overflow = null;
-    }
-}
