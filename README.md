116d7f9847dfa259da9fa54774e170526dec402a
niu-grandpa <2864103063@qq.com>
2020-12-18 11:08:07 +0800

    更新

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# 关于 Rabbit UI
+# 关于 Rabbit UI 重构
+
+> 2020.12.18 我决定对 RabbitUi 进行全面重构
+
--- src/mixins/clickoutside.js
+++ src/mixins/clickoutside.js
@@ -1,6 +1,6 @@
 /**
  * @param {HTMLElement} elem  目标元素
- * @param {Function} callback 点击的目标区域不包含目标元素的回调事件
+ * @param {function} callback 点击的目标区域不包含目标元素的回调事件
  */
 function clickOutSide(elem, callback) {
     document.addEventListener('click', e => {
