57cc39e180cadc80c60029e122de7606e5bad473
niu-grandpa <2864103063@qq.com>
2020-12-19 00:34:20 +0800

    更新

--- src/mixins/hasClass.ts
+++ src/mixins/hasClass.ts
@@ -1,4 +1,4 @@
-const hasClass = (elem: HTMLElement, cls: String) => {
+const hasClass = (elem: HTMLElement, cls: String): Boolean => {
   cls = cls || '';
   if (cls.replace(/\s/g, '').length == 0) return false;
   return new RegExp(' ' + cls + ' ').test(' ' + elem.className + ' ');
