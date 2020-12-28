3fbb8b72ae51b3e0bb61ec456d88249665510799
niu-grandpa <2864103063@qq.com>
2020-12-28 22:04:20 +0800

    更新

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -21,7 +21,7 @@ module.exports = merge(common, {
         new HtmlWebpackPlugin({
             inject: true,
             filename: path.resolve(__dirname, '../examples/dist/index.html'),
-            template: path.resolve(__dirname, '../examples/progress/index.html'),
+            template: path.resolve(__dirname, '../examples/timeline/index.html'),
         }),
         new FriendlyErrorsPlugin(),
     ],
